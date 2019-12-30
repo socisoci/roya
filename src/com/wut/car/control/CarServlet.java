d08d1e56d8f6860ca7e5da3ee4180b77b576515c
Administrator <Administrator@PC-12>
2019-12-30 17:21:19 +0800

    di三次

--- src/com/wut/car/control/CarServlet.java
+++ src/com/wut/car/control/CarServlet.java
@@ -1,17 +1,23 @@
 package com.wut.car.control;
 
 import java.io.IOException;
+
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.wut.car.model.CarDAO;
+
 /**
  * Servlet implementation class CarServlet
  */
 @WebServlet("/CarServlet")
 public class CarServlet extends HttpServlet {
+	private CarDAO dao=new CarDAO();
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request,response);
 	}
@@ -22,6 +28,14 @@ public class CarServlet extends HttpServlet {
 		case "listAll":
 		{
 			System.out.println("查询显示有的后台");
+			//调用dao方法查询所有的车辆信息
+			List<Car> cars=dao.listAll();
+			
+			System.out.println(cars.size());
+			//将查询出来的数据存储在request范围内
+			request.setAttribute("cars", cars);
+			//跳转到显示所有车辆的jsp页面
+			request.getRequestDispatcher("").forward(request, response);
 			break;
 		}
 		case "add":
--- src/com/wut/car/model/CarDAO.java
+++ src/com/wut/car/model/CarDAO.java
@@ -1,14 +1,35 @@
 package com.wut.car.model;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
+
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
 public class CarDAO {
 	/**
 	 * 这是查询显示所有的车辆的dao方法
 	 * @return
 	 */
-	public ArrayList<Car> listAll()
-	{
-		ArrayList<Car> cars=new ArrayList<>0;
+	public List<Car> listAll()
+	List<Car> cars=null;
+	try{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/Car","root","root");
+		QueryRunner run = new QueryRunner();
+		ResultSetHandler<List<Car>> h =new BeanHandler<Car>(Car.class);
+		cars= run.query(connection,"SELECT * FORM car",h);
+		
+	}
+	catch(Exception e){
+		cars=new ArrayList<>();
+		e.printStackTrace();
+	}
+	
 		return cars;
 		
 		
